43285fd885ceb4e4da6cc2d198b899fd06206958
Suraj Chandgude <surajchadgude0304@gmail.com>
2020-09-01 17:17:14 +0530

    Added lazy loading routes

--- src/router/index.js
+++ src/router/index.js
@@ -18,11 +18,36 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/brazil",
+    name: "Brazil",
+    component: () =>
+      import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue")
+  },
+  {
+    path: "/jamaica",
+    name: "Jamaica",
+    component: () =>
+      import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue")
+  },
+  {
+    path: "/panama",
+    name: "Panama",
+    component: () =>
+      import(/* webpackChunkName: "panama" */ "../views/Panama.vue")
+  },
+  {
+    path: "/hawaii",
+    name: "Hawaii",
+    component: () =>
+      import(/* webpackChunkName: "hawaai" */ "../views/Hawaai.vue")
   }
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: "history"
 });
 
 export default router;
